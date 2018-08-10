e6cf0cbbcb1949af1bf0deed598e4b9685440803
ScottWipp <scott.wipperfurth@gmail.com>
2018-08-10 18:42:04 -0400

    Updated Run.sh with 19 cores.

--- RunCodeswipp.sh
+++ RunCodeswipp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH -t 20
-#SBATCH --ntasks=8 
+#SBATCH -t 10
+#SBATCH --ntasks=19 
 #SBATCH --mem-per-cpu=4000	
 #SBATCH --share				
 #SBATCH --account=schmerr-lab-hi
 #SBATCH --mail-type=ALL
-#SBATCH --job-name=geoneutrino_test
+#SBATCH --job-name=geonu_test
 #SBATCH -L matlab
 . ~/.profile
 module load matlab
