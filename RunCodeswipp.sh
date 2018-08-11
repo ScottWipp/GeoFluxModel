ef7341444ce6322a00c54d88359317b493680faa
ScottWipp <scott.wipperfurth@gmail.com>
2018-08-10 21:09:01 -0400

    Changed account to schmerr-prj-hi

--- RunCodeswipp.sh
+++ RunCodeswipp.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+#SBATCH -L matlab
 #SBATCH -t 10
 #SBATCH --ntasks=19 
 #SBATCH --mem-per-cpu=4000	
 #SBATCH --share				
-#SBATCH --account=schmerr-lab-hi
+#SBATCH --account=schmerr-prj-hi
 #SBATCH --mail-type=ALL
 #SBATCH --job-name=geonu_test
-#SBATCH -L matlab
+
 . ~/.profile
 module load matlab
 matlab -nodisplay -nosplash -nodesktop -r "run('/lustre/swipp/code/CODE_LITHO1_CalcAbund_v3_Cluster.m'); exit"
